ab9c8917fc19b4717567edc5eb90d2fe1c49d59f
Matt Mohl <MattMohl@fullsail.edu>
2013-12-06 16:54:20 -0500

    [Delivers #61957496] Finished the mockup site. Seems to be able to connect to the api.

--- Mockup/test.js
+++ Mockup/test.js
@@ -1,5 +1,5 @@
 
-var testApp = angular.module('testApp', []);
+// var testApp = angular.module('testApp', []);
 
 var TestController = function TestController($scope, $http) {
 
@@ -8,9 +8,11 @@ var TestController = function TestController($scope, $http) {
 			method: 'GET',
 			url: 'http://api.remix.bestbuy.com/v1/products(search=PS4)?apiKey=kqwq2utctj7tpjur4jgq36k2'
 		}).success(function(data) {
-			$scope.$apply(function() {
-				$scope.results = angular.fromJson(data);
-			});
+			if(!$scope.$$phase) {
+				$scope.$apply(function() {
+					$scope.results = angular.fromJson(data);
+				});
+			};
 		});
 	};
 
