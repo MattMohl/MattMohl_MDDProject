97409c5f39241dc07fe06e123f007e1da599142b
Matt Mohl <MattMohl@fullsail.edu>
2013-12-10 02:30:23 -0500

    [#62116254] The form can now add users to Firebase

--- Site/js/user.js
+++ Site/js/user.js
@@ -2,8 +2,13 @@
 
 var User = function User($scope, $http) {
 
-	$scope.register = function($user, $pass) {
-		
+	$scope.register = function($user='', $pass='') {
+		console.log($user, $pass);
+		if($user != '' && $pass != '') {
+			var userRef = new Firebase('https://mdd-project.firebaseio.com/users/'+$user);
+			userRef.child('password').set($pass);
+			userRef.child('type').set('user');
+		}
 	};
 
 };
