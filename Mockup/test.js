f34c0aa523722ce195c313e21d33ee0c5765a5ba
Matt Mohl <MattMohl@fullsail.edu>
2013-12-12 17:55:14 -0500

    [Delivers #62430690] Did a static site where you can query products with pagination

--- Mockup/test.js
+++ Mockup/test.js
@@ -1,19 +1,20 @@
 
-// var testApp = angular.module('testApp', []);
+var testApp = angular.module('testApp', []);
 
 var TestController = function TestController($scope, $http) {
 
-	$scope.getData = function() {
-		$http({
-			method: 'GET',
-			url: 'http://api.remix.bestbuy.com/v1/products(search=PS4)?apiKey=kqwq2utctj7tpjur4jgq36k2'
-		}).success(function(data) {
-			if(!$scope.$$phase) {
-				$scope.$apply(function() {
-					$scope.results = angular.fromJson(data);
-				});
-			};
+	$scope.getData = function($query, $page) {
+		$http.jsonp( 'http://api.remix.bestbuy.com/v1/products(search='+$query+')?page='+$page+'&format=json&callback=JSON_CALLBACK&apiKey=kqwq2utctj7tpjur4jgq36k2')
+		.success(function(data, status, headers, config) {
+			console.log('success: ',data);
+			$scope.results = data.products;
+			$scope.pages = [];
+			for(var i=1; i<data.totalPages; i++) {
+				$scope.pages.push(i);
+			}
+		})
+		.error(function(data, status, headers, config) {
+			console.log('fail: '+data);
 		});
 	};
-
 };
